764a072143645866d1c2f674588583adedb1ac6a
jeancmr <jean.madiedo08@gmail.com>
2023-02-13 21:02:06 -0500

    derived state solution

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
 export default function Box(props) {
-  // <div className="box" key={item.id}></div>
-  return <div className="box"></div>;
+  const [on, setOn] = useState(props.on);
+  console.log(on);
+
+  const styles = {
+    backgroundColor: on ? "#222" : "transparent",
+  };
+
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
+
+  return <div onClick={toggle} style={styles} className="box"></div>;
 }
