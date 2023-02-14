55681983ab06455b396763c90fba64bc9f0dda7e
jeancmr <jean.madiedo08@gmail.com>
2023-02-13 21:26:24 -0500

    unified state

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,31 @@ function App() {
   const [allSquares, setAllSquare] = useState(boxes);
   console.log(allSquares);
 
+  function toggle(id) {
+    setAllSquare((prevState) => {
+      const newSquares = [];
+
+      for (let i = 0; i < prevState.length; i++) {
+        const currentSquare = prevState[i];
+
+        if (id === currentSquare.id) {
+          const updateSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updateSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+
+      return newSquares;
+    });
+  }
+
   const boxElements = allSquares.map((item) => (
     // <div className="box" key={item.id}></div>
-    <Box on={item.on} key={item.id} />
+    <Box on={item.on} key={item.id} toggle={() => toggle(item.id)} />
   ));
 
   return <main>{boxElements}</main>;
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-export default function Box(props) {
-  const [on, setOn] = useState(props.on);
-  console.log(on);
-
+export default function Box({ on, toggle }) {
   const styles = {
     backgroundColor: on ? "#222" : "transparent",
   };
-
-  function toggle() {
-    setOn((prevOn) => !prevOn);
-  }
 
   return <div onClick={toggle} style={styles} className="box"></div>;
 }
